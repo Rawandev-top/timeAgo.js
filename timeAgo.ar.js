d2b6a1d6fe540c467cdc1c4371df0e530598f908
Rawand <105124830+itsr4wand@users.noreply.github.com>
2023-11-18 20:55:15 +0100

    Create timeAgo.ar.js

---
+++ timeAgo.ar.js
@@ -0,0 +1,29 @@
+function TimeAgo() {
+  this.format = function (timestamp) {
+    const now = new Date();
+    const diff = now - timestamp;
+
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(months / 12);
+
+    if (years > 0) {
+      return 'منذ ' + years + ' سنة';
+    } else if (months > 0) {
+      return 'منذ ' + months + ' شهر';
+    } else if (days > 0) {
+      return 'منذ ' + days + ' يوم';
+    } else if (hours > 0) {
+      return 'منذ ' + hours + ' ساعة';
+    } else if (minutes > 0) {
+      return 'منذ ' + minutes + ' دقيقة';
+    } else if (seconds > 0) {
+      return 'منذ ' + seconds + ' ثانية';
+    } else {
+      return 'الآن';
+    }
+  };
+}
